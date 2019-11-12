83d2abd42fbe00247e8d856de7ff09c8aaf068f1
Yiran <percivalstr@163.com>
2019-11-12 15:55:09 +0800

    Create argo_test.go

---
+++ argo_test.go
@@ -0,0 +1,34 @@
+package arg
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewFromArgs(t *testing.T) {
+	v := []string{"User/x/main.go", "-n", "3", "--color", "yellow", "-d", "--dumb-long1", "--size", "small", "jojo"}
+	args := NewFromArgs(v)
+
+	ts := []bool{
+		args.Exist("-n"),
+		args.Get("-n") == "3",
+		args.Exist("--color"),
+		args.Get("--color") == "yellow",
+		args.Exist("-d"),
+		args.Get("-d") == "",
+		args.Exist("--dumb-long1"),
+		args.Get("--dumb-long1") == "",
+		args.Exist("--size"),
+		args.Get("--size") == "small",
+		args.Exist("jojo"),
+		args.Get("jojo") == "",
+	}
+
+	fmt.Println(args)
+
+	for _, o := range ts {
+		if !o {
+			t.Fail()
+		}
+	}
+}
